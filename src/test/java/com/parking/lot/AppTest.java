e2c10e7ca1bd9b17a54fb3b1f8dab9900c32345b
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-02-10 19:33:24 +0100

    init java

---
+++ src/test/java/com/parking/lot/AppTest.java
@@ -0,0 +1,11 @@
+package com.parking.lot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void shouldPass() {
+        assertTrue(true);
+    }
+}
